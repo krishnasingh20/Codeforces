0cefdbdbe60485fe121d5d32f061a4fdd65284af
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-09-26 01:13:52 +0530

    Create MEX_rose.java

---
+++ MEX_rose.java
@@ -0,0 +1,35 @@
+import java.util.*;
+ 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    while(t-- > 0) {
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+      HashMap<Integer, Integer> map = new HashMap<>();
+      for(int i = 0; i < n; i++) {
+        int x = sc.nextInt();
+        map.put(x, map.getOrDefault(x, 0)+1);
+      }
+      System.out.println(solve(n, map, k));
+    }
+    sc.close();
+  }
+  public static long solve(int n, HashMap<Integer, Integer> map, int k) {
+    int count = 0;
+    int i = 0;
+    while(i < k) {
+      if(map.containsKey(i) == false) {
+        count++;
+      }
+      i++;
+    }
+    if(map.getOrDefault(k, 0) == 0) {
+      return count;
+    }
+    else {
+      return Math.max(map.get(k), count);
+    }
+  }
+}
