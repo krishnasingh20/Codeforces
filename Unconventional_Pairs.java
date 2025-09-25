1da5fbe44146be2688864e8baf22b8738ad7d176
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-09-26 01:14:45 +0530

    Create Unconventional_Pairs.java

---
+++ Unconventional_Pairs.java
@@ -0,0 +1,26 @@
+import java.util.*;
+ 
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    while(t-- > 0) {
+      int n = sc.nextInt();
+      long[] arr = new long[n];
+      for(int i = 0; i < n; i++) {
+        arr[i] = sc.nextLong();
+      }
+      System.out.println(solve(n, arr));
+    }
+    sc.close();
+  }
+  public static long solve(int n, long[] arr) {
+    Arrays.sort(arr);
+    long ans = Long.MIN_VALUE;
+    for(int i = 0; i < n; i += 2) {
+      ans = Math.max(ans, (arr[i+1]-arr[i]));
+    }
+    return ans;
+  }
+  
+}
