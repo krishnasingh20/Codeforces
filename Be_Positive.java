2fc881ff1a81e702e910d81f5bcc326d7db9d907
Krishna Singh <krishnasinghks1225@gmail.com>
2025-09-26 21:27:11 +0530

    Be_Positive.java

--- Be_Positive.java
+++ Be_Positive.java
@@ -1,6 +1,6 @@
 import java.util.*;
  
-public class Main {
+public class Be_Positive {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int t = sc.nextInt();
