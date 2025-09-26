445c601b5c857ad6a8861c4c84d3a39b840a1a31
Krishna Singh <krishnasinghks1225@gmail.com>
2025-09-26 21:28:11 +0530

    The_Legend_of_Freya_the_Frog.java

--- The_Legend_of_Freya_the_Frog.java
+++ The_Legend_of_Freya_the_Frog.java
@@ -1,6 +1,6 @@
 import java.util.*;
  
-public class Main {
+public class The_Legend_of_Freya_the_Frog {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int t = sc.nextInt();
