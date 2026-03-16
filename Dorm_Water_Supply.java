263abb135a85a3530e0e414bec0fee732c6f2b69
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-17 01:26:01 +0530

    Update and rename Wow_factor.java to Dorm_Water_Supply.java

--- Dorm_Water_Supply.java
+++ Dorm_Water_Supply.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Wow_factor {
+public class Dorm_Water_Supply {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
